e191bde4b49bc6ff1738af5936bd2f021068c4f4
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-07-31 11:03:53 +0000

    Feat: Finished basic routes for the Users table.

---
+++ src/controller/users.controller.js
@@ -0,0 +1,43 @@
+const usersModel = require("../model/users.model");
+
+const usersController = {
+  index: async (req, res) => {
+    const results = await usersModel.select();
+    res.status(200);
+    res.json(results);
+  },
+  show: async (req, res) => {
+    const id = Number(req.params.id);
+    const results = await usersModel.selectId(id);
+    res.status(200);
+    res.json(results);
+  },
+  create: async (req, res) => {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+    };
+    const results = await usersModel.create(user);
+    res.status(200);
+    res.json(results);
+  },
+  update: async (req, res) => {
+    const id = Number(req.params.id);
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      updated_at: new Date().toISOString(),
+    };
+    const results = await usersModel.update(id, user);
+    res.status(200);
+    res.json(results);
+  },
+  destroy: async (req, res) => {
+    const id = Number(req.params.id);
+    const results = await usersModel.delete(id);
+    res.status(200);
+    res.json(results);
+  },
+};
+
+module.exports = usersController;
---
+++ src/model/users.model.js
@@ -0,0 +1,19 @@
+const usersModel = {
+  select: async () => {
+    return [];
+  },
+  selectId: async (id) => {
+    return {};
+  },
+  create: async (user) => {
+    return 0;
+  },
+  update: async (id, user) => {
+    return 0;
+  },
+  delete: async (id) => {
+    return 0;
+  },
+};
+
+module.exports = usersModel;
