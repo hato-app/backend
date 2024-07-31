a43b95fe2c585889540e334d25446f16111480cf
Brandon Cook <bcook2289@gmail.com>
2024-07-31 15:32:20 +0900

    feat: created cards_table migration

--- db/migrations/20240731060436_cards_table.js
+++ db/migrations/20240731060436_cards_table.js
@@ -2,10 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("cards", function (table) {
+    table.increments("id").primary();
+    table.integer("category_id");
+    table.date("created_date");
+    table.string("front_text", 255);
+    table.string("back_text", 255);
+    table.integer("views");
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("cards");
+};
