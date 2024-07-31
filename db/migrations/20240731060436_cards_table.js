87e50d75a17ac90c27306cb694c424eb0ada82eb
Brandon Cook <bcook2289@gmail.com>
2024-07-31 16:15:39 +0900

    feat: completed cards_likes and cards_dislikes

--- db/migrations/20240731060436_cards_table.js
+++ db/migrations/20240731060436_cards_table.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("cards", function (table) {
     table.increments("id").primary();
     table.integer("category_id");
-    table.date("created_date");
+    table.timestamps(true, true);
     table.string("front_text", 255);
     table.string("back_text", 255);
     table.integer("views");
--- db/migrations/20240731065615_card_dislikes.js
+++ db/migrations/20240731065615_card_dislikes.js
@@ -3,9 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.alterTable("cards", function (table) {
+  return knex.schema.createTable("cards_dislikes", function (table) {
     table.integer("user_id");
+    table.integer("card_id");
     table.foreign("user_id").references("id").inTable("users");
+    table.foreign("card_id").references("id").inTable("cards");
   });
 };
 
@@ -14,7 +16,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.alterTable("cards", function (table) {
-    table.dropColumn("user_id");
-  });
+  return knex.schema.dropTable("cards_dislikes");
 };
