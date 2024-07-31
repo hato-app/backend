a6222b7c43e916b5fc1681b3b27a739fb641609c
susheepls <169642127+susheepls@users.noreply.github.com>
2024-07-31 15:31:54 +0900

    Create users table in migrations

--- db/migrations/20240731060447_users_table.js
+++ db/migrations/20240731060447_users_table.js
@@ -3,7 +3,12 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', function (table) {
+    table.increments();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+    table.date('date_created').notNullable();
+  });
 };
 
 /**
@@ -11,5 +16,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('users');
 };
