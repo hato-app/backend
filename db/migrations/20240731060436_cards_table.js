8743ea2975f5206242f3de7c135a980f8344b207
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-01 05:55:32 +0000

    Fix: Migrations files now cascade delete. Feat: Finished basic users MVC.

--- db/migrations/20240731060436_cards_table.js
+++ db/migrations/20240731060436_cards_table.js
@@ -6,7 +6,17 @@ exports.up = function (knex) {
   return knex.schema.createTable("cards", function (table) {
     table.increments("id").primary();
     table.integer("category_id");
-    table.foreign("category_id").references('id').inTable('category');
+    table
+      .foreign("category_id")
+      .references("id")
+      .inTable("category")
+      .onDelete("SET NULL");
+    table.integer("user_id");
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("SET NULL");
     table.timestamps(true, true);
     table.string("front_text", 255);
     table.string("back_text", 255);
--- db/migrations/20240731065615_card_dislikes.js
+++ db/migrations/20240731065615_card_dislikes.js
@@ -4,10 +4,10 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("cards_dislikes", function (table) {
-    table.integer("user_id");
-    table.integer("card_id");
-    table.foreign("user_id").references("id").inTable("users");
-    table.foreign("card_id").references("id").inTable("cards");
+    table.integer("user_id").notNullable();
+    table.integer("card_id").notNullable();
+    table.foreign("user_id").references("id").inTable("users").onDelete("CASCADE");
+    table.foreign("card_id").references("id").inTable("cards").onDelete("CASCADE");
   });
 };
 
