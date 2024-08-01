5cd2ca2d51e8f7164354a41d65b212f645b4bb80
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-01 00:23:46 +0000

    Fix: Migration files.

--- db/seeds/005-likes.js
+++ db/seeds/005-likes.js
@@ -4,8 +4,8 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+  await knex('cards_likes').del()
+  await knex('cards_likes').insert([
     {
       user_id: "1",
       card_id: "2",
