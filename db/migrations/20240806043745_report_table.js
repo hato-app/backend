6b69ffc7281cd818b2ef44975438ce762a2b401c
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-06 16:18:12 +0900

    fix:Add foreign key reference to report tables

--- db/migrations/20240806043745_report_table.js
+++ db/migrations/20240806043745_report_table.js
@@ -8,20 +8,24 @@ exports.up = function (knex) {
       table.integer("card_id").notNullable();
       table.integer("user_id").notNullable();
       table.unique(["user_id", "card_id"]);
+      table.foreign("card_id").references("cards.id").onDelete("CASCADE").onUpdate("CASCADE");
+      table.foreign("user_id").references("users.id").onDelete("CASCADE").onUpdate("CASCADE");
     }),
     
     knex.schema.createTable("comments_report", function (table) {
       table.integer("comment_id").notNullable();
       table.integer("user_id").notNullable();
       table.unique(["comment_id", "user_id"]);
+      table.foreign("comment_id").references("comments.id").onDelete("CASCADE").onUpdate("CASCADE");
+      table.foreign("user_id").references("users.id").onDelete("CASCADE").onUpdate("CASCADE");
     }),
 
     knex.schema.alterTable("cards", function (table) {
-      table.boolean("flag_report").notNullable();
+      table.boolean("flag_report").notNullable().defaultTo(false);
     }),
 
     knex.schema.alterTable("comments", function (table) {
-      table.boolean("flag_report").notNullable();
+      table.boolean("flag_report").notNullable().defaultTo(false);
     }),
   ]);
 };
